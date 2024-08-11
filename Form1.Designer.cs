42b0e568bf0b16e8c883383a733f95c41bf61505
22123521 <22123521@ITWINLAB-8>
2024-08-11 19:02:00 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace DressDiscount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtPrecious = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMpho = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblFinalPricePrecious = new System.Windows.Forms.Label();
+            this.lblFinalPriceMpho = new System.Windows.Forms.Label();
+            this.lblPercentageSavedMpho = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Get 35.5% Discount";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Customer Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(183, 62);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // txtPrecious
+            // 
+            this.txtPrecious.Location = new System.Drawing.Point(226, 97);
+            this.txtPrecious.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPrecious.Name = "txtPrecious";
+            this.txtPrecious.Size = new System.Drawing.Size(128, 22);
+            this.txtPrecious.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Original Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Get R350 Discount";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Customer Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(183, 187);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(170, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Original Price";
+            // 
+            // txtMpho
+            // 
+            this.txtMpho.Location = new System.Drawing.Point(226, 227);
+            this.txtMpho.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMpho.Name = "txtMpho";
+            this.txtMpho.Size = new System.Drawing.Size(128, 22);
+            this.txtMpho.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(483, 40);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(296, 230);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblPercentageSavedMpho);
+            this.groupBox1.Controls.Add(this.lblFinalPriceMpho);
+            this.groupBox1.Controls.Add(this.lblFinalPricePrecious);
+            this.groupBox1.Location = new System.Drawing.Point(38, 298);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(741, 152);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 8F, System.Drawing.FontStyle.Italic);
+            this.button1.Location = new System.Drawing.Point(60, 470);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 62);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Palatino Linotype", 8F, System.Drawing.FontStyle.Italic);
+            this.button2.Location = new System.Drawing.Point(300, 470);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 62);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Palatino Linotype", 8F, System.Drawing.FontStyle.Italic);
+            this.button3.Location = new System.Drawing.Point(555, 470);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 62);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblFinalPricePrecious
+            // 
+            this.lblFinalPricePrecious.AutoSize = true;
+            this.lblFinalPricePrecious.Location = new System.Drawing.Point(8, 17);
+            this.lblFinalPricePrecious.Name = "lblFinalPricePrecious";
+            this.lblFinalPricePrecious.Size = new System.Drawing.Size(72, 15);
+            this.lblFinalPricePrecious.TabIndex = 16;
+            this.lblFinalPricePrecious.Text = "Original Price";
+            // 
+            // lblFinalPriceMpho
+            // 
+            this.lblFinalPriceMpho.AutoSize = true;
+            this.lblFinalPriceMpho.Location = new System.Drawing.Point(8, 71);
+            this.lblFinalPriceMpho.Name = "lblFinalPriceMpho";
+            this.lblFinalPriceMpho.Size = new System.Drawing.Size(72, 15);
+            this.lblFinalPriceMpho.TabIndex = 17;
+            this.lblFinalPriceMpho.Text = "Original Price";
+            // 
+            // lblPercentageSavedMpho
+            // 
+            this.lblPercentageSavedMpho.AutoSize = true;
+            this.lblPercentageSavedMpho.Location = new System.Drawing.Point(8, 113);
+            this.lblPercentageSavedMpho.Name = "lblPercentageSavedMpho";
+            this.lblPercentageSavedMpho.Size = new System.Drawing.Size(72, 15);
+            this.lblPercentageSavedMpho.TabIndex = 18;
+            this.lblPercentageSavedMpho.Text = "Original Price";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 382);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtMpho);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrecious);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Palatino Linotype", 8F, System.Drawing.FontStyle.Italic);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPrecious;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMpho;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblPercentageSavedMpho;
+        private System.Windows.Forms.Label lblFinalPriceMpho;
+        private System.Windows.Forms.Label lblFinalPricePrecious;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DressDiscount
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Input Variables
+            double originalPricePrecious = Convert.ToDouble(txtPrecious.Text);
+            double originalPriceMpho = Convert.ToDouble(txtMpho.Text);
+            double discountMpho = 350.0;
+
+            // Constants
+            double discountPercentagePrecious = 0.355;
+            double vatRate = 0.14;
+
+            // Intermediate Calculations for Precious
+            double discountAmountPrecious = originalPricePrecious * discountPercentagePrecious;
+            double priceAfterDiscountPrecious = originalPricePrecious - discountAmountPrecious;
+            double vatAmountPrecious = priceAfterDiscountPrecious * vatRate;
+            double finalPricePrecious = priceAfterDiscountPrecious + vatAmountPrecious;
+
+            // Intermediate Calculations for Mpho
+            double priceAfterDiscountMpho = originalPriceMpho - discountMpho;
+            double vatAmountMpho = priceAfterDiscountMpho * vatRate;
+            double finalPriceMpho = priceAfterDiscountMpho + vatAmountMpho;
+            double percentageSavedMpho = (discountMpho / originalPriceMpho) * 100.0;
+
+            // Display Output
+            lblFinalPricePrecious.Text = $"Precious bought a dress for {originalPricePrecious} and got a discount of {discountAmountPrecious} now he final Due is R" + finalPricePrecious.ToString("0.00");
+            lblFinalPriceMpho.Text = $"Mpho bough a dress for {originalPriceMpho} and got a discount of {discountMpho} and the final due is R" + finalPriceMpho.ToString("0.00");
+            lblPercentageSavedMpho.Text = $"Mpho saved" + percentageSavedMpho.ToString("0.00") + "%";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtPrecious.Clear();
+            txtMpho.Clear();
+            lblFinalPricePrecious.Text = "";
+            lblFinalPriceMpho.Text = "";
+            lblPercentageSavedMpho.Text = "";
+        }
+    }
+    
+}
